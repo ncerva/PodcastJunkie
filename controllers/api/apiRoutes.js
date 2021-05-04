4602b92dbe267145f03a0b0382a5f8ff4e045c93
Reid Johnson <rjsa210@gmail.com>
2021-05-03 20:04:29 -0700

    working on search form submit -RJ

--- controllers/api/apiRoutes.js
+++ controllers/api/apiRoutes.js
@@ -1,5 +1,47 @@
 const router = require('express').Router();
+const { Client } = require('podcast-api');
 
+
+//search by key word
+// router.get('/search/:id', (req, res) => {
+//   const client = Client({ apiKey: process.env.API_KEY });
+//   client.search({
+//     q: req.params.id,
+//     sort_by_date: 0,
+//     type: 'podcast',
+//     offset: 0,
+//     published_after: 0,
+//     language: 'English',
+//     safe_mode: 0,
+//   }).then((response) => {
+//     // Get response json data here
+//     console.log(response.data);
+//   }).catch((error) => {
+//     console.log(error)
+//   });
+// });
+
+
+// If apiKey is null, then we will connect to a mock server
+// that returns fake data for testing purposes.
+
+// const client = Client({ apiKey: process.env.API_KEY });
+// client.search({
+//   q: 'murder',
+//   sort_by_date: 0,
+//   type: 'podcast',
+//   offset: 0,
+//   published_after: 0,
+//   language: 'English',
+//   safe_mode: 0,
+// }).then((response) => {
+//   // Get response json data here
+//   console.log(response.data);
+// }).catch((error) => {
+//   console.log(error)
+// });
+
+//search by genre
 
 
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 
 // Login route
-router.get('/login', (req, res) => {
+router.get('/', (req, res) => {
     if (req.session.loggedIn) {
       res.redirect('/');
       return;
@@ -12,5 +12,12 @@ router.get('/login', (req, res) => {
     res.render('login');
   });
   
-
+  
+router.get('/search', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('search');
+});
 module.exports = router;
