f416c7f2dbab3598e0b8ebe825a5c00a4d1f7a43
Reid Johnson <rjsa210@gmail.com>
2021-05-06 21:31:39 -0700

    search working with URL -RJ

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,12 +26,12 @@ module.exports = router;
 
 
 
-router.get('/results', (req, res) => {
+router.post('/results', (req, res) => {
   // const keyword = JSON.parse(localStorage.getItem('keyword'));
   // const genre = JSON.parse(localStorage.getItem('genres'));
   try {
     const searchResults = client.search({
-      q: 'music',
+      q: req.body.keyWord,
       sort_by_date: 0,
       type: 'podcast',
       offset: 0,
@@ -42,7 +42,7 @@ router.get('/results', (req, res) => {
     })
       .then((response) => {
         const podcasts = response.data.results;
-        // res.json(podcasts);
+        // res.json(podcasts);  //uncomment this line for insomnia test
         res.render('results', {
           podcasts
         });
@@ -52,6 +52,59 @@ router.get('/results', (req, res) => {
     res.status(500).json(err);
   }
 });
-
+router.get('/results/:keyword', (req, res) => {
+  // const keyword = JSON.parse(localStorage.getItem('keyword'));
+  // const genre = JSON.parse(localStorage.getItem('genres'));
+  try {
+    const searchResults = client.search({
+      q: req.params.keyword,
+      sort_by_date: 0,
+      type: 'podcast',
+      offset: 0,
+      published_after: 0,
+      only_in: 'title,description',
+      language: 'English',
+      safe_mode: 0,
+    })
+      .then((response) => {
+        const podcasts = response.data.results;
+        // res.json(podcasts);  //uncomment this line for insomnia test
+        res.render('results', {
+          podcasts
+        });
+      });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+/*
+router.get('/results', (req, res) => {
+  // const keyword = JSON.parse(localStorage.getItem('keyword'));
+  // const genre = JSON.parse(localStorage.getItem('genres'));
+  try {
+    const searchResults = client.search({
+      q: req.body.keyWord,
+      sort_by_date: 0,
+      type: 'podcast',
+      offset: 0,
+      published_after: 0,
+      only_in: 'title,description',
+      language: 'English',
+      safe_mode: 0,
+    })
+      .then((response) => {
+        const podcasts = response.data.results;
+        // res.json(podcasts);  //uncomment this line for insomnia test
+        res.render('results', {
+          podcasts
+        });
+      });
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
+});
+*/
 
 module.exports = router;
--- public/js/search.js
+++ public/js/search.js
@@ -4,22 +4,40 @@ const searchSubmitBtn = document.querySelector('#search-submit');
 const keywordInput = document.querySelector('#keyword-input');
 const genreSelect = document.querySelector('#genre-select');
 
+// const searchHandler = (event) => {
+//   event.preventDefault();
 
+//   const keyword = keywordInput.value.trim();
+//   const genres = genreSelect.value.trim();
 
-const searchHandler = (event) => {
+//   if (!keyword) {
+//     alert('Must enter valid keyword!');
+//   } else {
+//   localStorage.setItem('keyword', JSON.stringify(keyword));
+//   localStorage.setItem('genres', JSON.stringify(genres));
+//   location.replace('/results');
+//   keywordInput.value = '';
+//   }
+// }
+
+const searchHandler = async (event) => {
   event.preventDefault();
 
-  const keyword = keywordInput.value.trim();
-  const genres = genreSelect.value.trim();
+  const keyWord = keywordInput.value.trim();
 
-  if (!keyword) {
-    alert('Must enter valid keyword!');
-  } else {
-  localStorage.setItem('keyword', JSON.stringify(keyword));
-  localStorage.setItem('genres', JSON.stringify(genres));
-  location.replace('/results');
-  keywordInput.value = '';
+  if (keyWord) {
+    const response = await fetch(`/results/${keyWord}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      console.log(response);
+    } else {
+      alert('Invalid search!');
+    }
   }
-}
+};
 
 searchSubmitBtn.addEventListener('click', searchHandler);
